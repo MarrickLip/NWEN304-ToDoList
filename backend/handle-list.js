91670fce8bda44f712818e107e431e8e3c0ee867
Marrick Lip <marricklip@me.com>
2019-09-09 22:54:53 +1200

    Begin building backend

---
+++ backend/handle-list.js
@@ -0,0 +1,7 @@
+module.exports.getList = (req, res) => {
+  res('getList')
+};
+
+module.exports.postList = (req, res) => {
+    res('postList');
+};
--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+const {postList} = require("./handle-list");
+const {getList} = require("./handle-list");
+
 router.get('/api', (req, res) => res.send('Yay!!'));
+
+router.get('/api/list', getList);
+router.post('/api/list', postList);
 
 module.exports = router;
