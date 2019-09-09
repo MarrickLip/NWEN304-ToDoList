2e76ebe0966e0b18540ba2076000feb5ad88ff03
Marrick Lip <marricklip@me.com>
2019-09-09 18:56:35 +1200

    Serve ASCII faces

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'frontend')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use(express.static(path.join(__dirname, 'frontend')))
+    //.set('views', path.join(__dirname, 'views'))
+    //.set('view engine', 'ejs')
+    //.get('/', (req, res) => res.render('pages/index'))
+    .get('/', (req, res) => res.send(cool()))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
