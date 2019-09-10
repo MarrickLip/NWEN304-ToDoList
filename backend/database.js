38d070cb7c90991a30a9a9ecafbdb3c41a6c116d
Marrick Lip <marricklip@me.com>
2019-09-10 15:02:44 +1200

    Implement GET list/{id} and idExists

--- backend/database.js
+++ backend/database.js
@@ -12,3 +12,17 @@ module.exports.query = async (query) => {
     const client = await pool.connect();
     return await client.query(query);
 };
+
+const buildTable = async () => {
+    try {
+        await module.exports.query('SELECT * FROM todoapp.items');
+        console.log('Table already exists.');
+    } catch (e) {
+        console.log('Building table...');
+        await module.exports.query('CREATE SCHEMA todoapp;');
+        await module.exports.query('CREATE TABLE todoapp.items (id text, title text, completed bool);');
+        await module.exports.query("INSERT INTO todoapp.items VALUES ('d290f1ee-6c54-4b01-90e6-d701748f0851', 'Walk the dog', false);");
+    }
+};
+
+buildTable();
--- backend/handle-list-id.js
+++ backend/handle-list-id.js
@@ -15,14 +15,22 @@ const idIsValid = (req, res) => {
     }
 };
 
-const idExists = (req, res) => {
-    return true;
+const idExists = async (req, res) => {
+    const { rows } = await db.query(`SELECT COUNT(*) FROM todoapp.items WHERE id='${req.params.id}';`);
+    if (rows[0].count === '1') {
+        return true
+    } else {
+        res.status(404).send({
+            code: 404,
+            message: 'todo item not found'
+        })
+    }
 };
 
 module.exports.getListId = async (req, res) => {
-    if (idIsValid(req, res)) {
-        const results = await db.query(`SELECT * FROM todoapp.items WHERE id="${req.params.id}";`);
-        res.send(results);
+    if (idIsValid(req, res) && await idExists(req, res)) {
+        const { rows } = await db.query(`SELECT * FROM todoapp.items WHERE id='${req.params.id}';`);
+        res.send(rows[0]);
     }
 };
 
